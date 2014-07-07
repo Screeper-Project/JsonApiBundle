beedbd3a81bcc5e8fa7a09f653470f4c0b4022e9
graille <astres3000@free.fr>
2014-07-08 00:42:12 +0200

    Set extension name

--- DependencyInjection/GLJsonApiExtension.php
+++ DependencyInjection/GLJsonApiExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-class GLJsonApiExtension extends Extension
+class GlJsonApiExtension extends Extension
 {
     /**
      * {@inheritDoc}
